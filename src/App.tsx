60276a65d2770a2c5eb403537c15af2c8e2cf6f9
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-07-26 18:51:05 +0530

    add connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,74 @@
-import React from 'react';
-import './App.css';
+import React,  { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+
+  const [walletAddress, setWalletAddress] = useState("");
+
+  useEffect(()=>{
+    getCurrentWalletConnected();
+    addWalletListener();
+  })
+
+  const connectWallet = async() => {
+    if(typeof window != "undefined"  && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({method : "eth_requestAccounts"});
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch(err: any){
+        console.log(err.message)
+      }
+      
+    }else {
+      console.log("Please Install Metamask")
+    }
+  }
+
+
+  const getCurrentWalletConnected = async() => {
+    if(typeof window != "undefined"  && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({method : "eth_accounts"});
+        if(accounts.length>0) {
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect Button")
+        }
+        
+      } catch(err: any){
+        console.log(err.message)
+      }
+      
+    }else {
+      console.log("Please Install Metamask")
+    }
+  }
+
+  const addWalletListener = async() => {
+    if(typeof window != "undefined"  && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts:any)=>{
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      })
+      
+    }else {
+      setWalletAddress("");
+      console.log("Please Install Metamask")
+    }
+  }
+
+
+
   return (
     <div>
-      <button>
-        Connect Wallet
+      <button onClick={connectWallet} >
+        {
+          walletAddress && walletAddress.length>0 ? `Connected : ${walletAddress.substring(0,6)}...${walletAddress.substring(38)}` : "Connect Wallet"
+        }
       </button>
     </div>
   );
 }
 
-export default App;
+
