0e817526d2baf8b520aacf5e67ed8403d06c10a0
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-07-26 16:27:28 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button>
+        Connect Wallet
+      </button>
+    </div>
+  );
+}
+
+export default App;
